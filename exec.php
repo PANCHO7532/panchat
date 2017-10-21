4a100685ceb3bf58d2444804710f91fef08b1ae6
PANCHO7532 <32972267+PANCHO7532@users.noreply.github.com>
2017-10-21 00:26:32 -0300

    Update exec.php
    Added comments :3

--- exec.php
+++ exec.php
@@ -1,7 +1,12 @@
 <?php
+//EXEC Core function of Panchat, here the major actions pass by this file
+//1. Collecting data:
 $author = $_POST["author"];
 $data = $_POST["data"];
+//Saving an cookie with the nickname, you can see the function later :3
 setcookie('author', $author, time()+36000);
+//Exceptions:
+//Here it is by default, if one condition its true, the message cannot reach to submit in DB.
 if($author == '') {
 	header("Location: index.php");
 	die;
@@ -14,11 +19,12 @@ if($data == '') {
 	header("Location: index.php");
 	die;
 }
-//include 'ban.txt';
+//Writing data to DB txt
 $archivo = "data.txt";
 $archivo = fopen($archivo, 'a');
 fwrite($archivo, "echo '<br>".$author."(".$_SERVER['REMOTE_ADDR'].")".": ".$data."</br>';\r\n");
 fwrite($archivo, "\r\n");
 fclose($archivo);
+//Redirecting to the main page
 header("Location: index.php");
 ?> 
